cb9690b463a4bbbb2b875fda520202a9126ac5b4
Antognia <168127637+Antognia@users.noreply.github.com>
2024-10-24 09:55:41 +0200

    Selfwork php 6 - aggiornato e corretto

--- index02.php
+++ index02.php
@@ -45,12 +45,12 @@ echo $tot/$count;
 
 for ($i = 1; $i <= 100; $i++) {
     if ($i % 3 == 0 && $i % 5 == 0) {
-        echo "HACKADEMY";
+        echo "HACKADEMY \n";
     } elseif ($i % 3 == 0) {
-        echo "PHP";
+        echo "PHP \n";
     } elseif ($i % 5 == 0) {
-        echo "JAVASCRIPT";
-    } 
+        echo "JAVASCRIPT \n";
+    }else { echo $i . "\n"
 }
 
 
